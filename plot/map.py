9e3f38075a2cf431083e603b78cd3896881b527a
GiantMolecularCloud <26749617+GiantMolecularCloud@users.noreply.github.com>
2020-09-17 12:29:37 +0200

    typo

--- plot/map.py
+++ plot/map.py
@@ -98,7 +98,7 @@ def map(fitsfile, **kwargs):
                     what type of overlay needs which information. Positions and lengths need to be
                     given as SkyCoord and astropy.units. The last element needs to be a kwarg dict
                     which can also be empty if you do not want to provide e.g. linewidth or style.
-        tests       List of text overlay elements. Each text overlay elements should give position,
+        texts       List of text overlay elements. Each text overlay elements should give position,
                     text (as a string) and a text formatting dictionary. The position can be a
                     SkyCoord object or a list of relative coordinates in the figure (e.g. [0.1,0.1]
                     for lower left corner). The dictionary with formatting information can be empty
