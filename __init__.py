4f2904fa7fe69c21a180d539e37ff0d2343d0f77
GiantMolecularCloud <26749617+GiantMolecularCloud@users.noreply.github.com>
2020-09-22 18:38:38 +0200

    warn at startup that aplpy is a complete mess

--- __init__.py
+++ __init__.py
@@ -30,3 +30,14 @@ helpers.hide_ComparisonWarnings()
 
 
 ###################################################################################################
+
+# warn that APLpy is a mess
+###########################
+
+if aplpy.version.major!=1:
+    raise Warning("easy_aplpy only partially works with APLpy 2 and the old APLpy 0 version. Some functionality will raise unsolvable errors.")
+if aplpy.version.version=='1.1.1':
+    raise Warning("APLpy 1.1.1 has problems plotting the beam but newer and older version have even more serious bugs.")
+
+
+###################################################################################################
