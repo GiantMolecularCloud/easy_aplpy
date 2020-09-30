7fdb778dfcab9a06e9a095cc66e6ceb29f5ce0a0
GiantMolecularCloud <26749617+GiantMolecularCloud@users.noreply.github.com>
2020-09-30 14:26:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,9 +36,9 @@ helpers.hide_ComparisonWarnings()
 
 import aplpy
 if aplpy.version.major!=1:
-    raise Warning("easy_aplpy only partially works with APLpy 2 and the old APLpy 0 version. Some functionality will raise unsolvable errors.")
+    print("\x1b[0;31;40m"+"easy_aplpy only partially works with APLpy 2 and the old APLpy 0 version. Some functionality will raise unsolvable errors."+"\x1b[0m")
 if aplpy.version.version=='1.1.1':
-    raise Warning("APLpy 1.1.1 has problems plotting the beam but newer and older version have even more serious bugs.")
+    print("\x1b[0;31;40m"+"APLpy 1.1.1 has problems plotting the beam but newer and older version have even more serious bugs."+"\x1b[0m")
 
 
 ###################################################################################################
